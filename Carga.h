4b23a5cd6d1859c819c66ebdf622808dff1096eb
agusleon <67339017+agusleon@users.noreply.github.com>
2020-07-23 20:03:08 -0300

    Objeto carga, tiene lo basico, después agrego a medida que vaya instanciando

---
+++ Carga.h
@@ -0,0 +1,63 @@
+#ifndef archivo_h
+#define archivo_h
+
+#include <stdio.h>
+#include <fstream>
+#include <string>
+
+//Archivos.h
+
+#include "Grafo.h"
+#include "ABB.h"
+
+using namespace std;
+
+class Carga
+{
+private:
+
+	//Atributos
+	
+	ifstream archivo_aeropuerto;
+	ifstream archivo_vuelos;
+	ABB* diccionario_aeropuerto;
+	Grafo* grafo_vuelos;
+	
+	//Metodos
+	
+	//PRE: Variable archivo de lectura por parametro.
+    //POST: Devuelve booleana depende de si se pudo abrir el archivo o no.
+    bool existe_archivo();
+
+
+public:
+
+    //Constructor
+    //PRE: Nombres de archivo .txt en string.
+    //POST: Variables de archivo ifstream creadas.
+    Archivo(){string nombre_archivo_aeropuerto, string nombre_archivo_vuelos};
+	
+    //Destructor
+    //PRE: Objeto archivo creado.
+    //Libera recursos, memoria, etc.
+    ~Archivo(){};
+	
+    
+    //PRE: -.
+    //POST: Arbol diccionario de aeropuertos creado mediante la lectura del aeropuerto.
+    bool cargar_diccionario_aeropuerto(){};
+	
+    //PRE: -.
+    //POST: Grafo de vuelos creado mediante la lectura del archivo de vuelos.
+    void cargar_grafo_vuelos(){};
+	
+	//PRE: -.
+	//POST: Devuelve un puntero al diccionario.
+	ABB* obtener_diccionario_aeropuerto(){};
+	
+	//PRE: -.
+	//POST: Devuelve puntero al grafo de vuelos.
+	Grafo* obtener_grafo_vuelos(){};
+};
+
+#endif /* archivo_hpp */
