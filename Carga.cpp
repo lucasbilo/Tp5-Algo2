a874796bd0c8f585464b12ab465d57ba9789dcdd
agusleon <67339017+agusleon@users.noreply.github.com>
2020-07-23 20:24:01 -0300

    Objeto Carga, FALTA

---
+++ Carga.cpp
@@ -0,0 +1,37 @@
+#include "Carga.h"
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+Carga::Carga(string nombre_archivo_aeropuertos, string nombre_archivo_vuelos)
+{
+	ifstream archivo_aeropuerto(nombre_archivo_aeropuertos);
+	ifstream archivo_vuelos(nombre_archivo_vuelos);
+	
+}
+
+bool Carga::existe_archivo(ifstream& archivo)
+{
+    if (!archivo.fail())
+    {
+        return true;
+    }
+	return false;
+}
+
+ABB* Carga::obtener_diccionario_aeropuerto()
+{
+	return diccionario
+
+
+//FALTA
+
+void Carga::cargar_diccionario_aeropuerto();
+
+
+void Carga::cargar_grafo_vuelos();
+
+
