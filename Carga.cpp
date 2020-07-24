efb7c8d6fa919481d09a2e9ee44418f69fd1bf45
agusleon <67339017+agusleon@users.noreply.github.com>
2020-07-23 21:54:50 -0300

    Metodo carga vuelos instanciado

--- Carga.cpp
+++ Carga.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 Carga::Carga(string nombre_archivo_aeropuertos, string nombre_archivo_vuelos)
 {
+	this -> nombre_archivo_aeropuertos  = nombre_archivo_aeropertos;
+	this -> nombre_archivo_vuelos = nombre_archivo_vuelos;
+	
 	ifstream archivo_aeropuerto(nombre_archivo_aeropuertos);
 	ifstream archivo_vuelos(nombre_archivo_vuelos);
 	diccionario_aeropuerto = NULL;
@@ -39,6 +42,34 @@ Grafo* Carga::obtener_grafo_vuelos()
 void Carga::cargar_diccionario_aeropuerto();
 
 
-void Carga::cargar_grafo_vuelos();
+bool Carga::cargar_grafo_vuelos()
+{
+	if (existe_archivo(archivo_grafo))
+	{
+		Grafo vuelos; //Objeto vuelos de clase Grafo creado
+		grafo_vuelos -> vuelos; //el puntero al grafo a punta el grafo propiamente dicho
+		
+		string codigo_IATA_partida, codigo_IATA_destino;
+		double costo_vuelo, horas_vuelo;
+		Lista<double>* aux_pesos = new Lista<double>;
+		
+		while (archivo_grafo >> codigo_IATA_partida)
+		{
+			archivo_grafo >> codigo_IATA_destino;
+			archivo_grafo >> costo_vuelo;
+			archivo_grafo >> horas_vuelo;
+			(*aux_pesos).insertar(costo_vuelo);
+			(*aux_pesos).insertar(horas_vuelo);
+			Arista* vuelo = new Arista(codigo_IATA_partida, codigo_IATA_destino, aux_pesos);
+			vuelos.agregar_arista(vuelo);
+		}
+		return true;
+		
+	else
+	{
+		cout << "El archivo " << nombre_archivo_vuelos << " no se pudo abrir, por favor comprobar que exista." << endl;
+		return false
+}
+	
 
 
