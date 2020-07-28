64bf06a874a4693d845365a1caeaa105076ad502
agusleon <67339017+agusleon@users.noreply.github.com>
2020-07-28 12:18:37 -0300

    Prueba de main

--- main.cpp
+++ main.cpp
@@ -4,9 +4,19 @@
 #include "Menu.h"
 #include "Validacion.h"
 
-
 using namespace std;
 
+const string NOMBRE_ARCHIVO_VUELOS = "vuelos.txt";
+const string NOMBRE_ARCHIVO_AEROPUERTOS = "aeropuertos.txt";
+
+//ESTO ES UNA PRUEBA
+
 int main() {
-  cout << "Hola mundo" <<endl;
+    
+    Carga carga(NOMBRE_ARCHIVO_AEROPUERTOS, NOMBRE_ARCHIVO_VUELOS);
+    if(carga.cargar_grafo_vuelos())
+    {
+        cout << "Se cargo el grafo" << endl;
+        ((carga.obtener_grafo_vuelos())->obtener_vertices())->imprimir_en_orden();
+    }
 }
