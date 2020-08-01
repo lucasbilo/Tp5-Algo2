1b414cb5ce7dfa490a3f8d5cf8c149b5d89ade1e
agusleon <67339017+agusleon@users.noreply.github.com>
2020-08-01 13:14:55 -0300

    Correccion de bugs (cuando ingresabas alguna letra, o dos palabras de nombre se rompia)

--- Validacion.cpp
+++ Validacion.cpp
@@ -3,16 +3,42 @@
 
 using namespace std;
 
+int Validacion::string_a_int(string palabra)
+{
+    int numero = atoi(palabra.c_str());
+    return numero;
+}
+
+bool Validacion::es_digito(string palabra)
+{
+    bool es_digito = true;
+    for (int i = 0; i < palabra.length(); i++)
+    {
+        if (!(isdigit(palabra[i])))
+        {
+            es_digito = false;
+        }
+    }
+    return es_digito;
+}
+
 int Validacion::opcion_entre_rangos(int min, int max) {
     int numero;
     cout << "Ingrese la opcion que desee realizar: ";
     cin >> numero;
-    while (numero < min || numero > max)
+    
+    bool digito = es_digito(numero);
+    
+    int numero_int = string_a_int(numero);
+    
+    while (numero_int < min || numero_int > max || (!digito))
     {
-        cout << "El numero ingresado esta fuera de rango, vuelva a ingresar un numero: ";
+        cout << "El numero ingresado esta fuera de rango o es invalido, vuelva a ingresar un numero: ";
         cin >> numero;
+        digito = es_digito(numero);
+        numero_int = string_a_int(numero);
     }
-    return numero;
+    return numero_int;
 }
 
 int Validacion::pedir_entero(std::string mensaje) {
@@ -26,5 +52,12 @@ string Validacion::pedir_string(std::string mensaje) {
     string palabra;
     cout << mensaje;
     cin >> palabra;
+    for (unsigned int i = 0; i <= palabra.length(); i++)
+    {
+        if (palabra[i] == ' ')
+        {
+            palabra[i] = '-';
+        }
+    }
     return palabra;
 }
