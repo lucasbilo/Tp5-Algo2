8b17d5e23a5bc6b661c1b0627bc49701e2e6c4b4
agusleon <67339017+agusleon@users.noreply.github.com>
2020-08-04 15:33:03 -0300

    Agrego exepcion para abrir archivos

--- Carga.cpp
+++ Carga.cpp
@@ -9,6 +9,7 @@
 #include "Diccionario.h"
 #include "NodoDic.h"
 #include "Aeropuertos.h"
+#include "Excepcion.h"
 
 using namespace std;
 
@@ -21,69 +22,69 @@ Carga::Carga(string nombre_archivo_aeropuertos, string nombre_archivo_vuelos)
 bool Carga::cargar_diccionario_aeropuerto(Diccionario<Aeropuerto>* diccionario_aeropuertos)
 {
 	ifstream archivo_aeropuerto(nombre_archivo_aeropuertos);
-	
-    if (!archivo_aeropuerto.fail())
-	{   
-        string codigo_IATA, nombre_aeropuerto, ciudad, pais;
-        float superficie;
-        unsigned int cant_terminales, d_nacionales, d_internacionales;
-		
-        while (archivo_aeropuerto >> codigo_IATA)
-        {
-            archivo_aeropuerto >> nombre_aeropuerto;
-            archivo_aeropuerto >> ciudad;
-            archivo_aeropuerto >> pais;
-            archivo_aeropuerto >> superficie;
-            archivo_aeropuerto >> cant_terminales;
-            archivo_aeropuerto >> d_nacionales;
-            archivo_aeropuerto >> d_internacionales;
-            Aeropuerto* aeropuerto = new Aeropuerto(codigo_IATA, nombre_aeropuerto, ciudad, pais, superficie, cant_terminales, d_nacionales, d_internacionales);
-            diccionario_aeropuertos -> insertar(codigo_IATA, aeropuerto);
+	string codigo_IATA, nombre_aeropuerto, ciudad, pais;
+    	float superficie;
+    	unsigned int cant_terminales, d_nacionales, d_internacionales;
+    
+    	try
+    	{
+        	if (archivo_aeropuerto.fail()) throw Mi_excepcion();
+        	while (archivo_aeropuerto >> codigo_IATA)
+        	{
+            		archivo_aeropuerto >> nombre_aeropuerto;
+            		archivo_aeropuerto >> ciudad;
+            		archivo_aeropuerto >> pais;
+            		archivo_aeropuerto >> superficie;
+            		archivo_aeropuerto >> cant_terminales;
+            		archivo_aeropuerto >> d_nacionales;
+            		archivo_aeropuerto >> d_internacionales;
+            		Aeropuerto* aeropuerto = new Aeropuerto(codigo_IATA, nombre_aeropuerto, ciudad, pais, superficie, cant_terminales, d_nacionales, d_internacionales);
+            		diccionario_aeropuertos -> insertar(codigo_IATA, aeropuerto);
         }
-		
-	archivo_aeropuerto.close();
-        return true;
-		
-    }
-        
-    else
-    {
-        cout << "El archivo " << nombre_archivo_aeropuertos << " no se pudo abrir, por favor comprobar que exista." << endl;
-        return false;
-    }
+                
+        archivo_aeropuerto.close();
+    	}
+    	catch (exception& e)
+    	{
+        	cout << e.what();
+        	cout << nombre_archivo_aeropuertos << endl;
+        	return false;
+    	}
+    
+    	return true;
 }
 
 bool Carga::cargar_grafo_vuelos(Grafo* grafo_vuelos)
 {
 	ifstream archivo_vuelos(nombre_archivo_vuelos);
-	
-	if (!archivo_vuelos.fail())
-	{
-		string codigo_IATA_partida, codigo_IATA_destino;
-		double costo_vuelo, horas_vuelo;
-		
-		while (archivo_vuelos >> codigo_IATA_partida)
-		{
-			Lista<double>* aux_pesos = new Lista<double>;
-			archivo_vuelos >> codigo_IATA_destino;
-			archivo_vuelos >> costo_vuelo;
-			archivo_vuelos >> horas_vuelo;
-			double* p_costo_vuelo = new double(costo_vuelo);
-			double* p_horas_vuelo = new double(horas_vuelo);
-			aux_pesos -> insertar(p_costo_vuelo);
-			aux_pesos -> insertar(p_horas_vuelo);
-			grafo_vuelos -> agregar_arista(codigo_IATA_partida, codigo_IATA_destino, aux_pesos);
-		}
-		
-		archivo_vuelos.close();
-		return true;
-    }
-		
-	else
-	{
-		cout << "El archivo " << nombre_archivo_vuelos << " no se pudo abrir, por favor comprobar que exista." << endl;
-        return false;
-    }
+    	string codigo_IATA_partida, codigo_IATA_destino;
+    	double costo_vuelo, horas_vuelo;
+    
+    	try
+    	{
+        	if (archivo_vuelos.fail()) throw Mi_excepcion();
+        	while (archivo_vuelos >> codigo_IATA_partida)
+        	{
+            		Lista<double>* aux_pesos = new Lista<double>;
+            		archivo_vuelos >> codigo_IATA_destino;
+            		archivo_vuelos >> costo_vuelo;
+            		archivo_vuelos >> horas_vuelo;
+            		double* p_costo_vuelo = new double(costo_vuelo);
+            		double* p_horas_vuelo = new double(horas_vuelo);
+            		aux_pesos -> insertar(p_costo_vuelo);
+            		aux_pesos -> insertar(p_horas_vuelo);
+            		grafo_vuelos -> agregar_arista(codigo_IATA_partida, codigo_IATA_destino, aux_pesos);
+        	}
+        	archivo_vuelos.close();
+
+    	}
+    	catch (exception& e)
+    	{
+        	cout << e.what();
+        	cout << nombre_archivo_vuelos << endl;
+        	return false;
+    	}
+	return true;
 }
 	
 
