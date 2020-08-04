13844e90c5b9cadec3a305c58cbf505e6105fa15
agusleon <67339017+agusleon@users.noreply.github.com>
2020-08-04 15:36:44 -0300

    cambio main para que si hay excepcion, se corte el programa

--- main.cpp
+++ main.cpp
@@ -10,17 +10,19 @@ using namespace std;
 const string NOMBRE_ARCHIVO_VUELOS = "vuelos.txt";
 const string NOMBRE_ARCHIVO_AEROPUERTOS = "aeropuertos.txt";
 
-//ESTO ES UNA PRUEBA
-
 int main() {
 
     Carga carga(NOMBRE_ARCHIVO_AEROPUERTOS, NOMBRE_ARCHIVO_VUELOS);
     Diccionario<Aeropuerto>* aeropuerto = new Diccionario<Aeropuerto>;
-    carga.cargar_diccionario_aeropuerto(aeropuerto);
     Grafo* vuelos = new Grafo();
-    carga.cargar_grafo_vuelos(vuelos);
-    Menu menu(aeropuerto, vuelos);
-    menu.menu_completo();
+    bool archivo_aeropuertos_abierto = carga.cargar_diccionario_aeropuerto(aeropuerto);
+    bool archivo_vuelos_abierto = carga.cargar_grafo_vuelos(vuelos);
+    if (archivo_aeropuertos_abierto && archivo_vuelos_abierto)
+    {
+       Menu menu(aeropuerto, vuelos);
+       menu.menu_completo(); 
+    }
+    
     delete vuelos;
     delete aeropuerto;
 }
